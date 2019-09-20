e6cd6c560c97b7cd0ec5f986cc8efc666ba6b152
Andrew Capp <acapp909@gmail.com>
2019-09-20 01:21:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+number_1 = 10 
+number_2 = 2 
+
+sum = number_1 + number_2
+difference = number_1 - number_2
+product = number_1 * number_2
+quotient = number_1 / number_2
